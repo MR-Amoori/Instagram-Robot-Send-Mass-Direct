067f71580ae76c090d4cc506db2576c6875b4118
Mohamad Reza Amoori <mohamad.reza.amoori99@gmail.com>
2022-06-07 11:44:45 +0430

    Add project files.

---
+++ test_bot/Form1.Designer.cs
@@ -0,0 +1,257 @@
+﻿namespace test_bot
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.cmbUsers = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtUsername = new System.Windows.Forms.TextBox();
+            this.txtPassword = new System.Windows.Forms.TextBox();
+            this.btnLoginAll = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtMeassage = new System.Windows.Forms.TextBox();
+            this.btnSend = new System.Windows.Forms.Button();
+            this.btnAdd = new System.Windows.Forms.Button();
+            this.txtDirect = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.dgvUsers = new System.Windows.Forms.DataGridView();
+            this.Number = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Username = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Succeeded = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvUsers)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // cmbUsers
+            // 
+            this.cmbUsers.FormattingEnabled = true;
+            this.cmbUsers.Location = new System.Drawing.Point(77, 12);
+            this.cmbUsers.Name = "cmbUsers";
+            this.cmbUsers.Size = new System.Drawing.Size(180, 21);
+            this.cmbUsers.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(18, 20);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(44, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Users : ";
+            // 
+            // txtUsername
+            // 
+            this.txtUsername.Location = new System.Drawing.Point(77, 39);
+            this.txtUsername.Name = "txtUsername";
+            this.txtUsername.Size = new System.Drawing.Size(180, 21);
+            this.txtUsername.TabIndex = 1;
+            // 
+            // txtPassword
+            // 
+            this.txtPassword.Location = new System.Drawing.Point(77, 65);
+            this.txtPassword.Name = "txtPassword";
+            this.txtPassword.Size = new System.Drawing.Size(180, 21);
+            this.txtPassword.TabIndex = 2;
+            // 
+            // btnLoginAll
+            // 
+            this.btnLoginAll.Location = new System.Drawing.Point(12, 120);
+            this.btnLoginAll.Name = "btnLoginAll";
+            this.btnLoginAll.Size = new System.Drawing.Size(245, 23);
+            this.btnLoginAll.TabIndex = 4;
+            this.btnLoginAll.Text = "Login All";
+            this.btnLoginAll.UseVisualStyleBackColor = true;
+            this.btnLoginAll.Click += new System.EventHandler(this.button1_Click_1);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(10, 152);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(62, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Username :";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 68);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(60, 13);
+            this.label3.TabIndex = 1;
+            this.label3.Text = "Password :";
+            // 
+            // txtMeassage
+            // 
+            this.txtMeassage.Location = new System.Drawing.Point(13, 175);
+            this.txtMeassage.Multiline = true;
+            this.txtMeassage.Name = "txtMeassage";
+            this.txtMeassage.Size = new System.Drawing.Size(245, 70);
+            this.txtMeassage.TabIndex = 6;
+            // 
+            // btnSend
+            // 
+            this.btnSend.Location = new System.Drawing.Point(12, 251);
+            this.btnSend.Name = "btnSend";
+            this.btnSend.Size = new System.Drawing.Size(245, 23);
+            this.btnSend.TabIndex = 7;
+            this.btnSend.Text = "Send";
+            this.btnSend.UseVisualStyleBackColor = true;
+            this.btnSend.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // btnAdd
+            // 
+            this.btnAdd.Location = new System.Drawing.Point(12, 91);
+            this.btnAdd.Name = "btnAdd";
+            this.btnAdd.Size = new System.Drawing.Size(245, 23);
+            this.btnAdd.TabIndex = 3;
+            this.btnAdd.Text = "Add";
+            this.btnAdd.UseVisualStyleBackColor = true;
+            this.btnAdd.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // txtDirect
+            // 
+            this.txtDirect.Location = new System.Drawing.Point(77, 149);
+            this.txtDirect.Name = "txtDirect";
+            this.txtDirect.Size = new System.Drawing.Size(180, 21);
+            this.txtDirect.TabIndex = 5;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(10, 42);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(62, 13);
+            this.label4.TabIndex = 1;
+            this.label4.Text = "Username :";
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.dgvUsers);
+            this.groupBox1.Location = new System.Drawing.Point(263, 5);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(245, 269);
+            this.groupBox1.TabIndex = 8;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Users : ";
+            // 
+            // dgvUsers
+            // 
+            this.dgvUsers.AllowUserToAddRows = false;
+            this.dgvUsers.AllowUserToDeleteRows = false;
+            this.dgvUsers.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvUsers.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Number,
+            this.Username,
+            this.Succeeded});
+            this.dgvUsers.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.dgvUsers.Location = new System.Drawing.Point(3, 17);
+            this.dgvUsers.Name = "dgvUsers";
+            this.dgvUsers.ReadOnly = true;
+            this.dgvUsers.RowHeadersVisible = false;
+            this.dgvUsers.Size = new System.Drawing.Size(239, 249);
+            this.dgvUsers.TabIndex = 0;
+            // 
+            // Number
+            // 
+            this.Number.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.AllCells;
+            this.Number.HeaderText = "*";
+            this.Number.Name = "Number";
+            this.Number.ReadOnly = true;
+            this.Number.Width = 38;
+            // 
+            // Username
+            // 
+            this.Username.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
+            this.Username.HeaderText = "UserName";
+            this.Username.Name = "Username";
+            this.Username.ReadOnly = true;
+            // 
+            // Succeeded
+            // 
+            this.Succeeded.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.AllCells;
+            this.Succeeded.HeaderText = "Succeeded";
+            this.Succeeded.Name = "Succeeded";
+            this.Succeeded.ReadOnly = true;
+            this.Succeeded.Width = 84;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(520, 280);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.txtMeassage);
+            this.Controls.Add(this.btnSend);
+            this.Controls.Add(this.btnAdd);
+            this.Controls.Add(this.btnLoginAll);
+            this.Controls.Add(this.txtDirect);
+            this.Controls.Add(this.txtPassword);
+            this.Controls.Add(this.txtUsername);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.cmbUsers);
+            this.Font = new System.Drawing.Font("Tahoma", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Bot Instagram";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.groupBox1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.dgvUsers)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox cmbUsers;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtUsername;
+        private System.Windows.Forms.TextBox txtPassword;
+        private System.Windows.Forms.Button btnLoginAll;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtMeassage;
+        private System.Windows.Forms.Button btnSend;
+        private System.Windows.Forms.Button btnAdd;
+        private System.Windows.Forms.TextBox txtDirect;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.DataGridView dgvUsers;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Number;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Username;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Succeeded;
+    }
+}
+
---
+++ test_bot/Form1.cs
@@ -0,0 +1,178 @@
+﻿using InstagramApiSharp;
+using InstagramApiSharp.API;
+using InstagramApiSharp.API.Builder;
+using InstagramApiSharp.Classes;
+using InstagramApiSharp.Classes.Models;
+using InstagramApiSharp.Logger;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Xml;
+
+namespace test_bot
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        List<User_Co> users = new List<User_Co>();
+        List<UserSessionData> sessions = new List<UserSessionData>();
+        List<IInstaApi> instas = new List<IInstaApi>();
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            cmbUsers.SelectedText = "User 1";
+
+            for (int i = 1; i <= 100; i++)
+            {
+                cmbUsers.Items.Add($"User {i.ToString()}");
+            }
+        }
+
+        int count = 1;
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            User_Co user = new User_Co()
+            {
+                Name = cmbUsers.SelectedItem.ToString(),
+                Username = txtUsername.Text,
+                Password = txtPassword.Text
+            };
+            users.Add(user);
+            dgvUsers.Rows.Add(count, txtUsername.Text);
+            count++;
+            cmbUsers.SelectedIndex++;
+        }
+
+        private async void button1_Click_1(object sender, EventArgs e)
+        {
+            cmbUsers.Enabled = false;
+            txtUsername.Enabled = false;
+            txtPassword.Enabled = false;
+            txtMeassage.Enabled = false;
+            txtDirect.Enabled = false;
+            btnAdd.Enabled = false;
+            btnLoginAll.Enabled = false;
+            btnSend.Enabled = false;
+
+            for (int i = 0; i < users.Count; i++)
+            {
+                UserSessionData data = new UserSessionData();
+                try
+                {
+                    data.UserName = users[i].Username;
+                    data.Password = users[i].Password;
+
+                }
+                catch
+                {
+                    continue;
+                }
+
+                sessions.Add(data);
+            }
+
+            int countSucceeded = 0;
+
+            foreach (var item in sessions)
+            {
+                var result = InstaApiBuilder.CreateBuilder()
+               .SetUser(item)
+               .UseLogger(new DebugLogger(LogLevel.All))
+               .SetRequestDelay(RequestDelay
+               .FromSeconds(0, 1)).Build();
+                var loginRequest = await result.LoginAsync();
+                if (loginRequest.Succeeded)
+                {
+                    instas.Add(result);
+                    dgvUsers.Rows[countSucceeded].Cells["Succeeded"].Value = "True";
+                }
+                else
+                {
+                    dgvUsers.Rows[countSucceeded].Cells["Succeeded"].Value = "False";
+                }
+                countSucceeded++;
+                await Patience();
+            }
+
+            MessageBox.Show("Logins Succeeded ...");
+
+            cmbUsers.Enabled = true;
+            txtUsername.Enabled = true;
+            txtPassword.Enabled = true;
+            txtMeassage.Enabled = true;
+            txtDirect.Enabled = true;
+            btnAdd.Enabled = true;
+            btnLoginAll.Enabled = true;
+            btnSend.Enabled = true;
+
+        }
+
+        private async void button2_Click(object sender, EventArgs e)
+        {
+
+            cmbUsers.Enabled = false;
+            txtUsername.Enabled = false;
+            txtPassword.Enabled = false;
+            txtMeassage.Enabled = false;
+            txtDirect.Enabled = false;
+            btnAdd.Enabled = false;
+            btnLoginAll.Enabled = false;
+            btnSend.Enabled = false;
+
+            try
+            {
+                var user = await instas[0].UserProcessor.GetUserAsync(txtDirect.Text);
+                foreach (var item in instas)
+                {
+                    var direct = await item.MessagingProcessor.SendDirectTextAsync(user.Value.Pk.ToString(), null, txtMeassage.Text);
+                    await Patience();
+                }
+                MessageBox.Show("Send All Directs ...");
+            }
+            catch
+            {
+
+                MessageBox.Show("با همه دایرکت نشد");
+            }
+
+            cmbUsers.Enabled = true;
+            txtUsername.Enabled = true;
+            txtPassword.Enabled = true;
+            txtMeassage.Enabled = true;
+            txtDirect.Enabled = true;
+            btnAdd.Enabled = true;
+            btnLoginAll.Enabled = true;
+            btnSend.Enabled = true;
+
+        }
+
+        private Task Patience()
+        {
+            int await = 2000;
+            return Task.Run(() =>
+            {
+                System.Threading.Thread.Sleep(await);
+            });
+        }
+    }
+
+    public class User_Co
+    {
+        public string Name { get; set; }
+        public string Username { get; set; }
+        public string Password { get; set; }
+    }
+}
